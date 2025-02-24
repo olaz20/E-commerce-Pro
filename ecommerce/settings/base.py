b996dbe1fe38a2938f3b730f66ae5d559f6a8dfc
olamide <olamoyegunolamidesammy@gmail.com>
2025-02-24 15:14:18 +0100

    edit url

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "django_filters",
+    "drf_spectacular",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
     "store",
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -18,28 +18,10 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path, re_path
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="NitHub Ecommerce Project API Documentation",
-        default_version='v1',
-        description="API documentation using Swagger",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="olamoyegunolamidesammy@gmail.com"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("userauth.urls")) ,
     path("accounts/", include("userauth.urls")),
     path("seller/", include("seller.urls")),
     path("store/", include("store.urls")),
